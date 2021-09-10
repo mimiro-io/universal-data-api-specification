308a4cbb6cbccc9b8269f406a0b114c58663e2cb
Graham Moore <gramoore@outlook.com>
2021-09-10 08:37:38 +0200

    added bs and html formats. updated build file

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 curl https://api.csswg.org/bikeshed/ -F file=@universal-data-api.bs -F force=1 > universal-data-api.html
+
+curl https://api.csswg.org/bikeshed/ -F file=@entity-graph-constraint-language.bs -F force=1 > entity-graph-constraint-language.html
+
